385bfea506575f8f862a82b0756434a89b4aeefd
Mariam Saad <163761374+mariam-2324@users.noreply.github.com>
2024-08-16 01:34:42 -0700

    Time: 49 ms (16.61%), Space: 17.7 MB (7.94%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -3,12 +3,14 @@ class Solution:
         ans = []
         for i in range(1, n+1):
             s = ''
+            if (i % 3 != 0) and (i % 5 != 0):
+                s += str(i)
             if (i % 3 == 0):
                 s += 'Fizz'
             if (i % 5 == 0):
                 s += 'Buzz'
-            if (s == ''):
-                s += str(i)
+            # if (s == ''):
+                # s += str(i)
             ans.append(s)
         return ans
 
