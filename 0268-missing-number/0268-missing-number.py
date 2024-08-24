a44a72052d69e1db3ee93a96bba8b4f2d737fe83
Mariam Saad <163761374+mariam-2324@users.noreply.github.com>
2024-08-24 06:34:41 -0700

    Time: 111 ms (58.63%), Space: 17.7 MB (51.02%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,12 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        num_set = set(nums)
-        n = len(nums) + 1
-        for i in range(n):
-            if i not in num_set:
-                return i
+        # num_set = set(nums)
+        # n = len(nums) + 1
+        # for i in range(n):
+        #     if i not in num_set:
+        #         return i
+
+        #Optimized sol as per the problem's requirement.
+         expected_sum = len(nums) * (len(nums) + 1) // 2
+         actual_sum = sum(nums)
+         return expected_sum - actual_sum 
