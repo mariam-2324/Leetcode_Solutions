05de53add40ca6889722313847e2215cc47928d0
Mariam Saad <163761374+mariam-2324@users.noreply.github.com>
2024-08-29 00:20:48 -0700

    Time: 31 ms (95.48%) | Memory: 16.6 MB (20.31%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.py
@@ -0,0 +1,16 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        s,e = 1, n
+        res = 0
+
+        while s <= e:
+            mid = (s + e) // 2
+            coins = (mid / 2) * (mid + 1)
+
+            if coins > n:
+                e = mid - 1
+            else:
+                s = mid + 1
+                res = max(res, mid)
+                
+        return res
